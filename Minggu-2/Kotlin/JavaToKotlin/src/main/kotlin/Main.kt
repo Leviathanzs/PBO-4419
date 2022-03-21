3133ad742c0792e5cc0e6d2f7fd828b846932aeb
Leviathanzs <okywidhiansyah@gmail.com>
2022-03-21 10:21:19 +0700

    Menambah file yang belum ter-upload

--- Minggu-2/Kotlin/JavaToKotlin/src/main/kotlin/Main.kt
+++ Minggu-2/Kotlin/JavaToKotlin/src/main/kotlin/Main.kt
@@ -1,15 +1,28 @@
+import kotlin.time.measureTimedValue
+
 class Mobil {
     var warna: String = ""
     var tahunProduksi: Int = 0
 }
-fun main(args: Array<String>) {
+class Mobil2{
+    var nyala: Int = 0
+    var mati: Int = 0
+    var gear: Int = 0
+
+}
+
+fun main() {
     //membuat Objek
     val mobilku = Mobil()
+    val mobilku2 = Mobil2()
 
     //memanggil atribut dan memberi nilai
     mobilku.warna = "Hitam"
     mobilku.tahunProduksi = 2006
+    mobilku2.gear = 1
 
     println("Warna: ${mobilku.warna}")
     println("Tahun: ${mobilku.tahunProduksi}")
+    println("Gear: ${mobilku2.gear}")
+
 }
