be8c8a254608d611806b858849dfb81b60fc0da8
Leviathanzs <okywidhiansyah@gmail.com>
2022-03-21 13:08:57 +0700

    Menambah class Mobil2

--- Minggu-2/Kotlin/JavaToKotlin/src/main/kotlin/Main.kt
+++ Minggu-2/Kotlin/JavaToKotlin/src/main/kotlin/Main.kt
@@ -1,28 +1,49 @@
-import kotlin.time.measureTimedValue
-
-class Mobil {
+class Mobil(){
     var warna: String = ""
-    var tahunProduksi: Int = 0
+    var tahun: Int = 0
+    fun warna(warna: String){
+        println("Warna: " + warna)
+    }
+    fun tahun(tahun: Int){
+        println("Tahun: " + tahun)
+    }
 }
 class Mobil2{
-    var nyala: Int = 0
-    var mati: Int = 0
     var gear: Int = 0
 
+    fun starter(mesin: Int){
+        if (mesin == 1){
+            println("Mesin Nyala")
+        }
+        else if(mesin == 0){
+            println("Mesin Mati")
+        }
+        else{
+            println("Error")
+        }
+    }
+
+    fun changeGearUp(up: Int){
+        gear = gear + up
+        println("Gear: "+ gear)
+    }
+    fun changeGearDown(down: Int){
+        gear = gear - down
+        println("Gear: "+ gear)
+    }
 }
 
 fun main() {
     //membuat Objek
     val mobilku = Mobil()
-    val mobilku2 = Mobil2()
+    val mobilDemo2 = Mobil2()
 
-    //memanggil atribut dan memberi nilai
-    mobilku.warna = "Hitam"
-    mobilku.tahunProduksi = 2006
-    mobilku2.gear = 1
-
-    println("Warna: ${mobilku.warna}")
-    println("Tahun: ${mobilku.tahunProduksi}")
-    println("Gear: ${mobilku2.gear}")
-
+    //Memanggil method dan menunju nilai parameter
+    mobilku.warna("Hitam")
+    mobilku.tahun(2006)
+    mobilDemo2.starter(1)
+    mobilDemo2.changeGearUp(1)
+    mobilDemo2.changeGearUp(1)
+    mobilDemo2.changeGearDown(1)
+    mobilDemo2.starter(0)
 }
