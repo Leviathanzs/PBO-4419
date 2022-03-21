932bc063ba9e88f2aba054249fa8a505ff26d663
Leviathanzs <okywidhiansyah@gmail.com>
2022-03-21 12:09:35 +0700

    Menambah file tugas program matematika

---
+++ Minggu-3/Kotlin/Matematika/src/main/kotlin/Main.kt
@@ -0,0 +1,32 @@
+class Matematika {
+    var tambah: Int = 0
+    var kurang: Int = 0
+    var kali: Int = 0
+    var bagi: Int = 0
+
+    fun pertambahan(x: Int, y: Int){
+        tambah = x+y
+        println("Pertambahan    : "+ x + "+" + y + "=" + tambah)
+    }
+    fun pengurangan(x: Int, y: Int){
+        kurang = x-y
+        println("Pengurangan    : "+ x + "-" + y + "=" + kurang)
+    }
+    fun perkalian(x: Int, y: Int){
+        kali = x+y
+        println("Perkalian      : "+ x + "*" + y + "=" + kali)
+    }
+    fun pembagian(x: Int, y: Int){
+        bagi = x+y
+        println("Pembagian      : "+ x + "/" + y + "=" + bagi)
+    }
+}
+
+fun main(){
+        val mat = Matematika()
+
+        mat.pertambahan(20,20)
+        mat.pengurangan(10,5)
+        mat.perkalian(10,20)
+        mat.pembagian(20,2)
+}
