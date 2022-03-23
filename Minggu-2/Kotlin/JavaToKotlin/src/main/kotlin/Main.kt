5c38f25de76f0e4f15766654e11b523aa56019d3
Leviathanzs <okywidhiansyah@gmail.com>
2022-03-23 10:50:08 +0700

    Memangil class mobil ke class mobil2

--- Minggu-2/Kotlin/JavaToKotlin/src/main/kotlin/Main.kt
+++ Minggu-2/Kotlin/JavaToKotlin/src/main/kotlin/Main.kt
@@ -1,9 +1,9 @@
-class Mobil{
+open class Mobil {
     fun warna(warna: String) = println("Warna: $warna")
     fun tahun(tahun: Int) = println("Tahun: $tahun")
 }
-class Mobil2{
-    private var gear: Int = 0
+class Mobil2 : Mobil() {
+    var gear: Int = 0
 
     fun starter(mesin: Int) = when (mesin) {
         1 -> {
@@ -29,15 +29,14 @@ class Mobil2{
 
 fun main() {
     //membuat Objek
-    val mobilku = Mobil()
-    val mobilDemo2 = Mobil2()
+    val mobilku = Mobil2()
 
     //Memanggil method dan menunju nilai parameter
     mobilku.warna("Hitam")
     mobilku.tahun(2006)
-    mobilDemo2.starter(1)
-    mobilDemo2.changeGearUp(1)
-    mobilDemo2.changeGearUp(1)
-    mobilDemo2.changeGearDown(1)
-    mobilDemo2.starter(0)
+    mobilku.starter(1)
+    mobilku.changeGearUp(1)
+    mobilku.changeGearUp(1)
+    mobilku.changeGearDown(1)
+    mobilku.starter(0)
 }
