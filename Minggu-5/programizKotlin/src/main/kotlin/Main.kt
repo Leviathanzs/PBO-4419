5d33c6c6c2fa721cfe29b53904dbeefd025eb76d
Leviathanzs <okywidhiansyah@gmail.com>
2022-04-03 21:01:32 +0700

    Add: Implementasi Kotlin Programiz

---
+++ Minggu-5/programizKotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,32 @@
+fun main(args: Array<String>) {
+    //Membuat variabel
+    val judul: String = "Implementasi Programiz Kotlin"
+    val bil1 = 10
+    val bil2 = 2.5
+    var hasil: Double
+    //----
+
+    //operator
+    hasil = bil1 + bil2
+    //----
+
+    //Expression
+    val score: Int
+    score = 21 + 21
+    //-------
+
+    //Conversion
+    val konversi: Int = bil2.toInt()
+    //------
+
+    //statement
+    val statement = 10 % 3
+    //------
+
+    //output
+    println(judul)
+    println("$bil1 + $bil2 = $hasil")
+    println("$score")
+    println("$statement")
+    println(konversi)
+}
